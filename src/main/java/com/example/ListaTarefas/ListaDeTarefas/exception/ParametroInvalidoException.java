b3395f21e21d2088d521fb110e67c176c84512a6
Gusta-1705 <142459466+Gusta-1705@users.noreply.github.com>
2025-04-12 17:38:40 -0300

    Colocando comentários na ParametroInvalidoException.java

--- src/main/java/com/example/ListaTarefas/ListaDeTarefas/exception/ParametroInvalidoException.java
+++ src/main/java/com/example/ListaTarefas/ListaDeTarefas/exception/ParametroInvalidoException.java
@@ -4,7 +4,10 @@ package com.example.ListaTarefas.ListaDeTarefas.exception;
 
 // Exceção personalizada para argumentos inválidos
 public class ParametroInvalidoException extends RuntimeException {
+    // Construtor que recebe uma mensagem personalizada sobre o erro ocorrido
     public ParametroInvalidoException(String mensagem) {
+        // Chama o construtor da superclasse RuntimeException para definir a mensagem do erro
         super(mensagem);
     }
 }
+
